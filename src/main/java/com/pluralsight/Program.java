3fe131ccb028090320c3a53343efccba386f5bd3
mariannaaspuria <aspuria.mp@gmail.com>
2025-12-01 14:59:37 -0600

    update configurable name commit

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -3,8 +3,9 @@ package com.pluralsight;
 public class Program {
 
     public static void main(String[] args) {
-        String name = "Slim Shady";
-        System.out.println("Hi! My name is, what? My name is, who? / My name is, chka-chka : " + name);
+        String firstName = args[0];
+        String lastName = args[1];
+        System.out.println("Hi! My name is, what? My name is, who? / My name is, chka-chka : " + firstName + " " + lastName);
 
     }
 
